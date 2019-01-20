06c78f6a5757f6677beff023aa3fc5a751b8ddbe
robby <robby.dh59@gmail.com>
2019-01-20 20:18:52 +0700

    Robby first

--- publisher.js
+++ publisher.js
@@ -1,8 +1,14 @@
 var mqtt = require('mqtt');
-var client  = mqtt.connect('mqtt://192.168.1.183:1883');
+var  server_ip  ='localhost:1883'; // server ini sesuai dengan  host ip server jika kita mejalankan di komputer yang sama maka bisa diganti dengan  localhost
+// localhost:1883
+var client  = mqtt.connect('mqtt://'+server_ip);
+// akan dijalankan  ketika   client sudah terkonsi 
 client.on('connect', function () {
-setInterval(function() {
-client.publish('myTopic', '0');
-console.log('Message Sent');
-}, 5000);
+    // setIterval  akan dijalankan  setiap 2 detik sekali 
+    setInterval(function() {
+        // makan akan melakukan  publish pesan  '0' dengan  topic  : myTopic
+        client.publish('myTopic', '0');
+        // melakukkan  console.log mendakan  pesan terkirim 
+        console.log('Message Sent');
+}, 2000);
 });
--- subscriber.js
+++ subscriber.js
@@ -1,12 +1,18 @@
 var mqtt = require('mqtt')
-
-var client  = mqtt.connect('mqtt://192.168.1.183:1883')
+var  server_ip  ='localhost:1883'; // server ini sesuai dengan  host ip server jika kita mejalankan di komputer yang sama maka bisa diganti dengan  localhost
+// localhost:1883
+var client  = mqtt.connect('mqtt://'+server_ip)
+// akan dijalankan  ketika   client sudah terkonsi 
 client.on('connect', function () {
+// men-subcribe   topic  bernama myTopic
     client.subscribe('myTopic')
 })
+//akan dijalan ketika  mendapatkana pesan dari publisher
 client.on('message', function (topic, message) {
-context = message.toString();
-console.log(context)
+    
+    context = message.toString(); // mengubah  pesan  ke string 
+    // menampilakan pesan  yang dikirim  oleh publisher 
+    console.log(context)
 })
 
 
